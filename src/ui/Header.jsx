f9579c4d251919733fca4f23266b8665ef2aae8b
Harsh <harshjha8453@gmail.com>
2024-12-21 20:59:32 +0530

    feat: initialize React Router and implement API data fetching
    - Set up React Router DOM for client-side routing
    - Created a dummy backend to simulate API connectivity
    - Added multiple pages to demonstrate routing functionality
    - Implemented API data fetching for dynamic content

---
+++ src/ui/Header.jsx
@@ -0,0 +1,14 @@
+import { Link } from "react-router-dom"
+
+const Header = () => {
+  return (
+    <header>
+        <Link to='/'>
+            Fast React Pizza Co.
+        </Link>
+            <p>Harsh</p>
+    </header>
+  )
+}
+
+export default Header
